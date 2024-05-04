d151710ecce89472721e758449ceb154731a1065
Crinibus <crinibus.ignem@gmail.com>
2024-05-04 17:03:11 +0200

    Update team and mmr_test and add player

---
+++ backend/mmr/player.go
@@ -0,0 +1,8 @@
+package mmr
+
+import "github.com/mafredri/go-trueskill"
+
+type Player struct {
+	Initials string
+	Player   trueskill.Player
+}
--- backend/test/mmr_test.go
+++ backend/test/mmr_test.go
@@ -3,34 +3,99 @@ package main
 import (
 	"fmt"
 	"mmr/backend/mmr"
+	team "mmr/backend/mmr"
 	"os"
 	"testing"
 
 	"github.com/mafredri/go-trueskill"
 )
 
+func createNewPlayer(initials string) mmr.Player {
+	return mmr.Player{
+		Initials: initials,
+		Player:   trueskill.NewPlayer(trueskill.DefaultMu, trueskill.DefaultSigma),
+	}
+}
+
+func createTeam(matchTeam mmr.MatchTeam) *mmr.Team {
+	p1 := createNewPlayer(matchTeam.Member1)
+	p2 := createNewPlayer(matchTeam.Member2)
+
+	return &mmr.Team{
+		Players: []mmr.Player{p1, p2},
+		Score:   int16(matchTeam.Score),
+	}
+}
+
+func getAverageTeamPlayer(team *mmr.Team) trueskill.Player {
+	averageMu := team.GetAverageMu()
+	averageSigma := team.GetAverageSigma()
+
+	return trueskill.NewPlayer(averageMu, averageSigma)
+}
+
+func calculateNewMMR(ts trueskill.Config, team1 *mmr.Team, team2 *mmr.Team) (mmr.Team, mmr.Team) {
+	avgTeam1 := getAverageTeamPlayer(team1)
+	avgTeam2 := getAverageTeamPlayer(team2)
+
+	fmt.Printf("Team1:\n")
+	fmt.Printf("- Player1: %s - mu: %v\n", team1.Players[0].Initials, team1.Players[0].Player.Mu())
+	fmt.Printf("- Player2: %s - mu: %v\n\n", team1.Players[1].Initials, team1.Players[1].Player.Mu())
+	fmt.Printf("Team2:\n")
+	fmt.Printf("- Player1: %s - mu: %v\n", team2.Players[0].Initials, team2.Players[0].Player.Mu())
+	fmt.Printf("- Player2: %s - mu: %v\n\n", team2.Players[1].Initials, team2.Players[1].Player.Mu())
+
+	fmt.Printf("AvgTeam1: %s\n", avgTeam1)
+	fmt.Printf("AvgTeam2: %s\n\n", avgTeam2)
+
+	var winner trueskill.Player
+	var loser trueskill.Player
+	if team1.Score > team2.Score {
+		winner = avgTeam1
+		loser = avgTeam2
+	} else {
+		winner = avgTeam2
+		loser = avgTeam1
+	}
+
+	teams := []trueskill.Player{winner, loser}
+	teamsWithNewSkills, _ := ts.AdjustSkills(teams, false)
+
+	newTeam1 := teamsWithNewSkills[0]
+	newTeam2 := teamsWithNewSkills[1]
+
+	diffTeam1 := avgTeam1.Mu() - newTeam1.Mu()
+	diffTeam2 := avgTeam2.Mu() - newTeam2.Mu()
+
+	fmt.Printf("Team1: %s\n", newTeam1)
+	fmt.Printf("Team2: %s\n", newTeam2)
+	fmt.Printf("Team1 - Diff mu: %f\n", diffTeam1)
+	fmt.Printf("Team2 - Diff mu: %f\n", diffTeam2)
+	fmt.Println()
+
+	return mmr.Team{}, mmr.Team{}
+}
+
 func TestMMRCalculation(t *testing.T) {
-
 	ts := trueskill.New(trueskill.DrawProbabilityZero())
-	p1 := ts.NewPlayer()
-	p2 := trueskill.NewPlayer(p1.Mu()+5, p1.Sigma())
-	p3 := ts.NewPlayer()
-	p4 := ts.NewPlayer()
 
-	team1 := mmr.NewTeam([]trueskill.Player{p1, p2})
-	team2 := mmr.NewTeam([]trueskill.Player{p3, p4})
+	match := team.Match{
+		Team1: team.MatchTeam{
+			Score:   10,
+			Member1: "nila",
+			Member2: "msth",
+		},
+		Team2: team.MatchTeam{
+			Score:   8,
+			Member1: "mbla",
+			Member2: "maan",
+		},
+	}
 
-	avgPlayer1 := trueskill.NewPlayer(team1.GetMu(), team1.GetSigma())
-	avgPlayer2 := trueskill.NewPlayer(team2.GetMu(), team2.GetSigma())
+	team1 := createTeam(match.Team1)
+	team2 := createTeam(match.Team2)
 
-	skills := []trueskill.Player{avgPlayer1, avgPlayer2}
-
-	newSkills, _ := ts.AdjustSkills(skills, false)
-	avgnew1 := newSkills[0]
-	avgnew2 := newSkills[1]
-
-	fmt.Println("team1 1:", avgPlayer1.Mu()-avgnew1.Mu())
-	fmt.Println("team2 2:", avgPlayer2.Mu()-avgnew2.Mu())
+	calculateNewMMR(ts, team1, team2)
 }
 
 func TestMain(m *testing.M) {

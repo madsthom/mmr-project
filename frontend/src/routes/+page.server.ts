3e7bfb05d955fed80ef695980fd8938e27e85d09
Mads Smed <msth@trifork.com>
2024-05-04 23:34:42 +0200

    style(frontend): Test that backend action does not trigger in frontend folder

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -4,17 +4,16 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
   try {
-    const res = await apiClient.leaderboardApi.statsLeaderboardGet();
-    
+    const entries = await apiClient.leaderboardApi.statsLeaderboardGet();
+
     return {
-      leaderboardEntries: res.toSorted((a, b) => (b.mmr ?? 0) - (a.mmr ?? 0)),
+      leaderboardEntries: entries.toSorted(
+        (a, b) => (b.mmr ?? 0) - (a.mmr ?? 0)
+      ),
     };
   } catch (error) {
     return fail(500, {
       message: 'Failed to load leaderboard',
     });
   }
-
-
 };
-

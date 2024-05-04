d30e717910626b8db9156043e634817f1fbe4f24
Mads-Bo Lassen <mbla@trifork.com>
2024-05-04 17:05:00 +0200

    feat(frontend): :sparkles: ui improvements
    Co-authored-by: Mathias Andresen <mathias@3mogs.dk>

--- frontend/src/routes/submit/+page.server.ts
+++ frontend/src/routes/submit/+page.server.ts
@@ -1,10 +1,33 @@
+import { apiClient } from '$lib/server/api/apiClient';
+import { fail } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 import { matchSchema } from './match-schema';
 
 export const load: PageServerLoad = async () => {
   return {
     form: await superValidate(zod(matchSchema)),
   };
+};
+
+export const actions: Actions = {
+  default: async (event) => {
+    const form = await superValidate(event, zod(matchSchema));
+
+    if (!form.valid) {
+      return fail(400, {
+        form,
+      });
+    }
+
+    try {
+      const res = await apiClient.mmrApi.mmrMatchPost({ match: form.data });
+      return { res };
+    } catch (error) {
+      return fail(500, {
+        error,
+      });
+    }
+  },
 };

7bee4aa971c9e0bff05aa6543247ae5589c41b3e
Tim Hjermitslev Keren Gjøderum <6584419+lajtmaN@users.noreply.github.com>
2024-05-24 11:29:45 +0200

    Started on a statistics page

---
+++ frontend/src/routes/statistics/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import PageTitle from '$lib/components/page-title.svelte';
+  import {
+    LineChart,
+    ScaleTypes,
+    type ChartTabularData,
+    type LineChartOptions,
+  } from '@carbon/charts-svelte';
+  import '@carbon/charts-svelte/styles.css';
+
+  const data: ChartTabularData = [
+    {
+      player: 'thj',
+      date: '2021-01-01',
+      rating: 1000,
+    },
+    {
+      player: 'thj',
+      date: '2021-01-02',
+      rating: 1010,
+    },
+    { player: 'bhs', date: '2021-01-02', rating: 1100 },
+    { player: 'bhs', date: '2021-01-05', rating: 1090 },
+    { player: 'bhs', date: '2021-01-06', rating: 1095 },
+  ];
+  const options: LineChartOptions = {
+    theme: 'g100',
+    curve: 'curveMonotoneX',
+    data: {
+      groupMapsTo: 'player',
+      loading: false,
+    },
+    axes: {
+      left: {
+        title: 'Rating',
+        mapsTo: 'rating',
+        scaleType: ScaleTypes.LINEAR,
+        includeZero: false,
+      },
+      bottom: {
+        title: 'Time',
+        mapsTo: 'date',
+        scaleType: ScaleTypes.TIME,
+      },
+    },
+  };
+</script>
+
+<PageTitle>Statistics</PageTitle>
+<LineChart {data} {options} />

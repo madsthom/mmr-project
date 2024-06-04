589597096e36dc1e3905f69f2d567f14e4346bd5
Bjarke Hesthaven Søndergaard <bhsgaard@gmail.com>
2024-06-04 16:47:04 +0200

    feat: Refactor statistics page to use sorted chart data

--- frontend/src/routes/(authed)/statistics/+page.svelte
+++ frontend/src/routes/(authed)/statistics/+page.svelte
@@ -1,22 +1,25 @@
 <script lang="ts">
   import PageTitle from '$lib/components/page-title.svelte';
   import {
-      LineChart,
-      ScaleTypes,
-      type ChartTabularData,
-      type LineChartOptions,
+    LineChart,
+    ScaleTypes,
+    type ChartTabularData,
+    type LineChartOptions,
   } from '@carbon/charts-svelte';
   import '@carbon/charts-svelte/styles.css';
   import type { PageData } from './$types';
 
   export let data: PageData;
 
-  const chartData: ChartTabularData = data.statistics?.map(stat => ({
-    player: stat.name,
-    date: stat.date,
-    rating: stat.mmr
-  })) ?? []
-  
+  const chartData: ChartTabularData =
+    data.statistics
+      ?.toSorted((a, b) => a.name.localeCompare(b.name))
+      .map((stat) => ({
+        player: stat.name,
+        date: stat.date,
+        rating: stat.mmr,
+      })) ?? [];
+
   const options: LineChartOptions = {
     theme: 'g100',
     curve: 'curveMonotoneX',

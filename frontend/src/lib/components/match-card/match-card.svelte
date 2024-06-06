f8cbd23acae7e74460f7803e4515991f53471b84
Bjarke Hesthaven Søndergaard <bhsgaard@gmail.com>
2024-06-06 22:07:05 +0200

    feat: Improve match card layout and styling for mobile

--- frontend/src/lib/components/match-card/match-card.svelte
+++ frontend/src/lib/components/match-card/match-card.svelte
@@ -13,12 +13,12 @@
 </script>
 
 <Card.Root>
-  <div class="flex flex-row items-center gap-4 p-2 px-4">
+  <div class="flex flex-row items-center gap-1 px-2 py-1 md:px-4 md:py-2">
     <div
       class="flex flex-1 flex-row items-center gap-4"
       class:text-primary={match.team1.score === 10}
     >
-      <p class="min-w-[2ch] text-4xl font-extrabold">
+      <p class="min-w-[1.5ch] text-4xl font-extrabold">
         {match.team1.score === 0 ? '🥚' : match.team1.score}
       </p>
       <div class="flex flex-1 flex-col">
@@ -45,7 +45,7 @@
         <TeamMember {users} {match} {showMmr} team="team2" member="member1" />
         <TeamMember {users} {match} {showMmr} team="team2" member="member2" />
       </div>
-      <p class="min-w-[2ch] text-right text-4xl font-extrabold">
+      <p class="min-w-[1.5ch] text-right text-4xl font-extrabold">
         {match.team2.score === 0 ? '🥚' : match.team2.score}
       </p>
     </div>

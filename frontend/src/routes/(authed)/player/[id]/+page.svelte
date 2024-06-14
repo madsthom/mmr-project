52efc1177ba1388e2a6cfd50f4f4941070936aa0
Bjarke Hesthaven Søndergaard <bhsgaard@gmail.com>
2024-06-14 15:35:19 +0200

    Move to table of players instead

--- frontend/src/routes/(authed)/player/[id]/+page.svelte
+++ frontend/src/routes/(authed)/player/[id]/+page.svelte
@@ -4,166 +4,255 @@
   import PageTitle from '$lib/components/page-title.svelte';
   import * as Card from '$lib/components/ui/card';
   import LineChart from '$lib/components/ui/line-chart/line-chart.svelte';
+  import * as Table from '$lib/components/ui/table';
   import { isPresent } from '$lib/util/isPresent';
   import { Separator } from 'bits-ui';
   import type { PageData } from './$types';
 
   export let data: PageData;
 
-  $: teammates = data.matches
-    .map((match) => {
-      if (
-        match.team1.member1 === data.user.userId ||
-        match.team1.member2 === data.user.userId
-      ) {
-        return match.team1;
-      }
+  interface MemberLeaderboardEntry {
+    playerId: number;
+    wins: number;
+    losses: number;
+    total: number;
+  }
 
-      return match.team2;
-    })
-    .flatMap((team) => [team.member1, team.member2])
-    .filter(isPresent)
-    .reduce<Record<number, number>>((acc, player) => {
-      if (player === data.user.userId) {
+  $: teammates = Object.entries(
+    data.matches
+      .map((match) => {
+        if (
+          match.team1.member1 === data.user.userId ||
+          match.team1.member2 === data.user.userId
+        ) {
+          return match.team1;
+        }
+
+        return match.team2;
+      })
+      .reduce<Record<number, MemberLeaderboardEntry>>((acc, team) => {
+        const isWin = team.score === 10;
+        acc[team.member1] = {
+          ...acc[team.member1],
+          playerId: team.member1,
+          wins: (acc[team.member1]?.wins ?? 0) + (isWin ? 1 : 0),
+          losses: (acc[team.member1]?.losses ?? 0) + (isWin ? 0 : 1),
+          total: (acc[team.member1]?.total ?? 0) + 1,
+        };
+        acc[team.member2] = {
+          ...acc[team.member2],
+          playerId: team.member2,
+          wins: (acc[team.member2]?.wins ?? 0) + (isWin ? 1 : 0),
+          losses: (acc[team.member2]?.losses ?? 0) + (isWin ? 0 : 1),
+          total: (acc[team.member2]?.total ?? 0) + 1,
+        };
         return acc;
-      }
-      acc[player] = (acc[player] ?? 0) + 1;
-      return acc;
-    }, {});
+      }, {})
+  )
+    .filter(([_player, stats]) => stats.playerId !== data.user.userId)
+    .sort((a, b) => b[1].total - a[1].total)
+    .map(([_player, stats]) => stats)
+    .slice(0, 5);
 
-  $: opponents = data.matches
-    .map((match) => {
-      if (
-        match.team1.member1 !== data.user.userId &&
-        match.team1.member2 !== data.user.userId
-      ) {
+  $: opponents = Object.entries(
+    data.matches
+      .map((match) => {
+        if (
+          match.team1.member1 === data.user.userId ||
+          match.team1.member2 === data.user.userId
+        ) {
+          return match.team2;
+        }
+
         return match.team1;
-      }
-
-      return match.team2;
-    })
-    .flatMap((team) => [team.member1, team.member2])
-    .filter(isPresent)
-    .reduce<Record<number, number>>((acc, player) => {
-      if (player === data.user.userId) {
+      })
+      .reduce<Record<number, MemberLeaderboardEntry>>((acc, team) => {
+        const isWin = team.score === 10;
+        acc[team.member1] = {
+          ...acc[team.member1],
+          playerId: team.member1,
+          wins: (acc[team.member1]?.wins ?? 0) + (isWin ? 0 : 1),
+          losses: (acc[team.member1]?.losses ?? 0) + (isWin ? 1 : 0),
+          total: (acc[team.member1]?.total ?? 0) + 1,
+        };
+        acc[team.member2] = {
+          ...acc[team.member2],
+          playerId: team.member2,
+          wins: (acc[team.member2]?.wins ?? 0) + (isWin ? 0 : 1),
+          losses: (acc[team.member2]?.losses ?? 0) + (isWin ? 1 : 0),
+          total: (acc[team.member2]?.total ?? 0) + 1,
+        };
         return acc;
-      }
-      acc[player] = (acc[player] ?? 0) + 1;
-      return acc;
-    }, {});
-
-  $: mostPlayedOpponent = Object.entries(opponents).reduce<{
-    player: string;
-    count: number;
-  } | null>((acc, [player, count]) => {
-    if (acc == null || count > acc.count) {
-      return { player, count };
-    }
-
-    return acc;
-  }, null);
-
-  $: mostPlayedTeammate = Object.entries(teammates).reduce<{
-    player: string;
-    count: number;
-  } | null>((acc, [player, count]) => {
-    if (acc == null || count > acc.count) {
-      return { player, count };
-    }
-
-    return acc;
-  }, null);
+      }, {})
+  )
+    .sort((a, b) => b[1].total - a[1].total)
+    .map(([_player, stats]) => stats)
+    .slice(0, 5);
 </script>
 
-{#if data.user?.displayName}
-  <PageTitle>{data.user?.displayName} ({data.user?.name})</PageTitle>
-{:else}
-  <PageTitle>{data.user?.name}</PageTitle>
-{/if}
-
-<div class="mt-6 grid grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-2">
-  {#if data.stats.mmr != null}
-    <Kpi title="MMR">{data.stats.mmr}</Kpi>
+<div class="flex flex-col gap-6">
+  {#if data.user?.displayName}
+    <PageTitle>{data.user?.displayName} ({data.user?.name})</PageTitle>
+  {:else}
+    <PageTitle>{data.user?.name}</PageTitle>
   {/if}
-  <Kpi title="# Matches">
-    {data.stats.totalMatches}
-  </Kpi>
-  <Kpi title="# Wins">
-    {data.stats.wins}
-  </Kpi>
-  <Kpi title="# Losses">
-    {data.stats.lost}
-  </Kpi>
-  <Kpi title="Win rate">
-    {new Intl.NumberFormat(undefined, {
-      style: 'percent',
-      maximumFractionDigits: 1,
-    }).format(data.stats.winrate)}
-  </Kpi>
-  {#if data.stats.daysSinceLastMatch != null}
-    <Kpi title="Last match">
-      {new Intl.RelativeTimeFormat(undefined, {
-        style: 'narrow',
-        numeric: 'auto',
-      }).format(data.stats.daysSinceLastMatch, 'day')}
+
+  <div class="grid grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-2">
+    {#if data.stats.mmr != null}
+      <Kpi title="MMR">{data.stats.mmr}</Kpi>
+    {/if}
+    <Kpi title="# Matches">
+      {data.stats.totalMatches}
     </Kpi>
+    <Kpi title="# Wins">
+      {data.stats.wins}
+    </Kpi>
+    <Kpi title="# Losses">
+      {data.stats.lost}
+    </Kpi>
+    <Kpi title="Win rate">
+      {new Intl.NumberFormat(undefined, {
+        style: 'percent',
+        maximumFractionDigits: 1,
+      }).format(data.stats.winrate)}
+    </Kpi>
+    {#if data.stats.daysSinceLastMatch != null}
+      <Kpi title="Last match">
+        {new Intl.RelativeTimeFormat(undefined, {
+          style: 'narrow',
+          numeric: 'auto',
+        }).format(data.stats.daysSinceLastMatch, 'day')}
+      </Kpi>
+    {/if}
+  </div>
+
+  {#if data.mmrHistory.length > 0}
+    <h2 class="-mb-6 mt-6 text-2xl md:text-4xl">Rating over time</h2>
+
+    <LineChart
+      data={data.mmrHistory.map((stat) => ({
+        date: stat.date,
+        player: stat.name,
+        rating: stat.mmr,
+      }))}
+      height={300}
+      legend={false}
+    />
+  {/if}
+
+  {#if opponents.length > 0}
+    <Card.Root>
+      <Card.Content class="flex flex-col p-0 md:p-6">
+        <h2 class="text-xl md:text-2xl">Most common opponents</h2>
+        <Table.Root class="">
+          <Table.Header>
+            <Table.Row>
+              <!-- <Table.Head class="w-[3ch]">#</Table.Head> -->
+              <Table.Head class="">Player</Table.Head>
+              <Table.Head class="">
+                <span class="sm:hidden">W</span>
+                <span class="hidden sm:inline">Wins</span>
+              </Table.Head>
+              <Table.Head class="">
+                <span class="sm:hidden">L</span>
+                <span class="hidden sm:inline">Losses</span>
+              </Table.Head>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {#each opponents as { playerId, losses, wins }}
+              {@const playerUser = data.users?.find(
+                (user) => user.userId == playerId
+              )}
+              <Table.Row>
+                <!-- <Table.Cell class="max-w-[3ch] font-bold">{rank}</Table.Cell> -->
+                <Table.Cell>
+                  <div class="flex flex-col items-start">
+                    {#if playerUser?.displayName != null}
+                      <span class="hidden w-full truncate sm:block">
+                        {playerUser?.displayName}
+                      </span>
+                    {/if}
+                    <span class="block">{playerUser?.name}</span>
+                  </div>
+                </Table.Cell>
+                <Table.Cell>
+                  <div class="flex flex-row items-center gap-2">
+                    {wins}
+                  </div>
+                </Table.Cell>
+                <Table.Cell>
+                  <div class="flex flex-row items-center gap-2">
+                    {losses}
+                  </div>
+                </Table.Cell>
+              </Table.Row>
+            {/each}
+          </Table.Body>
+        </Table.Root>
+      </Card.Content>
+    </Card.Root>
+  {/if}
+  {#if teammates.length > 0}
+    <Card.Root>
+      <Card.Content class="flex flex-col p-0 md:p-6">
+        <h2 class="text-xl md:text-2xl">Most common teammates</h2>
+        <Table.Root class="">
+          <Table.Header>
+            <Table.Row>
+              <!-- <Table.Head class="w-[3ch]">#</Table.Head> -->
+              <Table.Head class="">Player</Table.Head>
+              <Table.Head class="">
+                <span class="sm:hidden">W</span>
+                <span class="hidden sm:inline">Wins</span>
+              </Table.Head>
+              <Table.Head class="">
+                <span class="sm:hidden">L</span>
+                <span class="hidden sm:inline">Losses</span>
+              </Table.Head>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {#each teammates as { playerId, losses, wins }}
+              {@const playerUser = data.users?.find(
+                (user) => user.userId == playerId
+              )}
+              <Table.Row>
+                <!-- <Table.Cell class="max-w-[3ch] font-bold">{rank}</Table.Cell> -->
+                <Table.Cell>
+                  <div class="flex flex-col items-start">
+                    {#if playerUser?.displayName != null}
+                      <span class="hidden w-full truncate sm:block">
+                        {playerUser?.displayName}
+                      </span>
+                    {/if}
+                    <span class="block">{playerUser?.name}</span>
+                  </div>
+                </Table.Cell>
+                <Table.Cell>
+                  <div class="flex flex-row items-center gap-2">
+                    {wins}
+                  </div>
+                </Table.Cell>
+                <Table.Cell>
+                  <div class="flex flex-row items-center gap-2">
+                    {losses}
+                  </div>
+                </Table.Cell>
+              </Table.Row>
+            {/each}
+          </Table.Body>
+        </Table.Root>
+      </Card.Content>
+    </Card.Root>
+  {/if}
+
+  {#if data.matches.length > 0}
+    <h2 class="my-6 text-2xl md:text-4xl">Matches</h2>
+    <div class="flex flex-1 flex-col items-stretch gap-2">
+      {#each data.matches ?? [] as match}
+        <MatchCard users={data.users ?? []} {match} showMmr />
+      {/each}
+    </div>
   {/if}
 </div>
-
-<Card.Root class="mt-6 flex flex-col gap-4 bg-gray-800 p-4 shadow-sm">
-  {#if mostPlayedOpponent != null}
-    {@const mostPlayedOpponentUser = data.users?.find(
-      (user) => user.userId.toString() === mostPlayedOpponent.player
-    )}
-    {#if mostPlayedOpponentUser != null}
-      <div class="flex flex-col gap-2">
-        <h3 class="text-lg text-gray-300">Biggest Enemy</h3>
-        <p class="text-primary font-bold">
-          {mostPlayedOpponentUser.displayName ?? mostPlayedOpponentUser.name} - {mostPlayedOpponent.count}
-          matches
-        </p>
-      </div>
-    {/if}
-  {/if}
-  <Separator.Root
-    orientation="horizontal"
-    class="bg-background shrink-0 data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-[1px]"
-  />
-  {#if mostPlayedTeammate != null}
-    {@const mostPlayedTeammateUser = data.users?.find(
-      (user) => user.userId.toString() === mostPlayedTeammate.player
-    )}
-    {#if mostPlayedTeammateUser != null}
-      <div class="flex flex-col gap-2">
-        <h3 class="text-lg text-gray-300">Best Friend</h3>
-        <p class="text-primary font-bold">
-          {mostPlayedTeammateUser.displayName ?? mostPlayedTeammateUser.name} - {mostPlayedTeammate.count}
-          matches
-        </p>
-      </div>
-    {/if}
-  {/if}
-</Card.Root>
-
-{#if data.mmrHistory.length > 0}
-  <h2 class="-mb-6 mt-6 text-2xl md:text-4xl">Rating over time</h2>
-
-  <LineChart
-    data={data.mmrHistory.map((stat) => ({
-      date: stat.date,
-      player: stat.name,
-      rating: stat.mmr,
-    }))}
-    height={300}
-    legend={false}
-  />
-{/if}
-
-{#if data.matches.length > 0}
-  <h2 class="my-6 text-2xl md:text-4xl">Matches</h2>
-  <div class="flex flex-1 flex-col items-stretch gap-2">
-    {#each data.matches ?? [] as match}
-      <MatchCard users={data.users ?? []} {match} showMmr />
-    {/each}
-  </div>
-{/if}

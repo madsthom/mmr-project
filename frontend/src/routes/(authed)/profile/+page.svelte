b32b4082bdfcaa658065f259d7acb988012135d3
Bjarke Hesthaven Søndergaard <bhsgaard@gmail.com>
2024-06-17 22:00:35 +0200

    Tweak design of toast

--- frontend/src/routes/(authed)/profile/+page.svelte
+++ frontend/src/routes/(authed)/profile/+page.svelte
@@ -1,19 +1,19 @@
 <script>
   import PageTitle from '$lib/components/page-title.svelte';
   import Button from '$lib/components/ui/button/button.svelte';
-  import * as Card from '$lib/components/ui/card';
   import { Info } from 'lucide-svelte';
 </script>
 
 <div class="flex flex-col gap-3">
   <PageTitle>Profile</PageTitle>
 
-  <Card.Root class="flex flex-col gap-3 border-white p-4">
-    <div class="flex space-x-2 text-white">
-      <Info /><span>You have not claimed a player yet</span>
+  <div class="border-l-primary flex space-x-2 border border-l-4 p-4">
+    <Info class="text-4xl text-white" size="1em" />
+    <div class="flex flex-1 flex-col space-y-2">
+      <span class="text-white">You have not claimed a player yet</span>
+      <Button href="/profile/claim">Claim a player</Button>
     </div>
-    <Button href="/profile/claim">Claim a player</Button>
-  </Card.Root>
+  </div>
 
   <form method="post" action="/signout" class="contents">
     <Button type="submit" variant="secondary">Logout</Button>
